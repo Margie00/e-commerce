a7550570a185cdf5e553fce1e4b7196f9044355b
Margie00 <margielpd@yahoo.com>
2024-12-22 00:51:57 -0500

    checkout function update

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,26 @@
-import { Children } from "react"
+import { Children, useContext, useEffect } from "react"
+import Product from "./Product";
+import { ProductsContext } from "./ProductsContext";
 
-export default function Layout(){
+export default function Layout(children){
+    const {setSelectedProducts} = useContext(ProductsContext);
+    const [success, setSuccess] = useState(false);
+    useEffect(() => {
+        if(window.location.href.includes('success')){
+            setSelectedProducts([]);
+            setSuccess(true);
+        }
+    }, []);
     return(
         <div>
             <div className="p-5" >
-                {Children}
+                {success && (
+                    <div className="mb-5 bg-blue-400 text-white text-lg p-5 rounded-xl">
+                        Thanks for your order!
+                    </div>
+                )}
+                {children}
              </div>
-             
             <Footer />
         </div>
         
